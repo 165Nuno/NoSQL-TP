005ea6c740015c3ee6a88e1ceafaf0793f496a36
pedrops164 <A95826@alunos.uminho.pt>
2024-05-28 13:42:42 +0100

    added print_results argument to print the results of the queries

--- Queries/compare.py
+++ Queries/compare.py
@@ -1,9 +1,9 @@
 from prettytable import PrettyTable
 
-from neo4j import run_query1_neo4j, run_query2_neo4j
-from sql import run_query1_sql, run_query2_sql
+from neo4j import run_query1_neo4j, run_query2_neo4j, run_query3_neo4j
+from sql import run_query1_sql, run_query2_sql, run_query3_sql
 
-def compare_queries(query_name: str, neo4j_results, sql_results):
+def compare_queries(query_name: str, neo4j_results, sql_results, print_results=False):
     # Convert lists of dictionaries to sets of tuples
     neo4j_set = {tuple(d.items()) for d in neo4j_results}
     sql_set = {tuple(d.items()) for d in sql_results}
@@ -13,7 +13,8 @@ def compare_queries(query_name: str, neo4j_results, sql_results):
 
     # Print the comparison result
     table = PrettyTable()
-    table.field_names = [f"Comparison Result {query_name}"]
+    table.title = "Comparison Result"
+    table.field_names = [query_name]
     table.add_row(["Equal" if match else "Not Equal"])
     print(table)
 
@@ -24,36 +25,71 @@ def compare_queries(query_name: str, neo4j_results, sql_results):
 
         if len(neo4j_only) > 0:
             diff_table_neo4j = PrettyTable()
+            diff_table_neo4j.title = "Different table Neo4j"
             neo4j_columns = list(neo4j_results[0].keys())
             diff_table_neo4j.field_names = ["Source"] + neo4j_columns
             for entry in neo4j_only:
                 entry_dict = dict(entry)
                 row = ["Neo4j"] + [entry_dict[col] for col in neo4j_columns]
                 diff_table_neo4j.add_row(row)
-            print("Different table neo4j:")
             print(diff_table_neo4j)
 
         if len(sql_only) > 0:
             diff_table_sql = PrettyTable()
+            diff_table_sql.title = "Different table Sql"
             diff_table_sql.field_names = ["Source"] + list(sql_results[0].keys())
             sql_columns = list(sql_results[0].keys())
             for entry in sql_only:
                 entry_dict = dict(entry)
                 row = ["SQL"] + [entry_dict[col] for col in sql_columns]
                 diff_table_sql.add_row(row)
-            print("Different table sql:")
             print(diff_table_sql)
 
-def compare_query1():
+    if print_results:
+        results_table_sql = PrettyTable()
+        results_table_sql.title = "Results Sql"
+        results_table_neo4j = PrettyTable()
+        results_table_neo4j.title = "Results Neo4j"
+        
+        if len(neo4j_results) > 0:
+            neo4j_columns = list(neo4j_results[0].keys())
+            results_table_neo4j.field_names = neo4j_columns
+            for entry in neo4j_results:
+                entry_dict = dict(entry)
+                row = [entry_dict[col] for col in neo4j_columns]
+                results_table_neo4j.add_row(row)
+        
+        if len(sql_results) > 0:
+            sql_columns = list(sql_results[0].keys())
+            results_table_sql.field_names = sql_columns
+            for entry in sql_results:
+                entry_dict = dict(entry)
+                row = [entry_dict[col] for col in sql_columns]
+                results_table_sql.add_row(row)
+
+        print(results_table_sql)
+        print(results_table_neo4j)
+
+
+
+
+def compare_query1(print_results=False):
     neo4j_results = run_query1_neo4j()
     sql_results = run_query1_sql()
-    compare_queries("Query 1", neo4j_results, sql_results)
+    compare_queries("Query 1", neo4j_results, sql_results, print_results)
 
-def compare_query2():
+# Compares Query to Get Patients with the Most Appointments
+def compare_query2(print_results=False):
     neo4j_results = run_query2_neo4j()
     sql_results = run_query2_sql()
-    compare_queries("Query 2", neo4j_results, sql_results)
+    compare_queries("Query 2", neo4j_results, sql_results, print_results)
+
+def compare_query3(print_results=False):
+    neo4j_results = run_query3_neo4j()
+    sql_results = run_query3_sql()
+    compare_queries("Query 3", neo4j_results, sql_results, print_results)
 
 if __name__ == "__main__":
     compare_query1()
     compare_query2()
+    compare_query3(True)
